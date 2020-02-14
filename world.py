03fefa2625a589979e227354c1e0031a5b364df7
Annika Wollschläger <annika.wollschlaeger@tum.de>
2020-02-14 15:48:52 +0100

    one plot for one world

--- world.py
+++ world.py
@@ -34,22 +34,18 @@ class SimpleWorld:
         for v in self.vehicles:
             v.vehicle.collisionFree = self.freeSpace
 
-    def initScene(self, ax):
-        for v in self.vehicles:
-            ax.add_patch(v.circle)
-            ax.add_patch(v.triangle)
-
-    def showScene(self, animate=False):
-        fig = plt.figure()
-        ax = plt.axes(xlim=(0, self.width), ylim=(
+        self.fig = plt.figure()
+        self.ax = plt.axes(xlim=(0, self.width), ylim=(
             0, self.height), xlabel='x', ylabel='y')
         plt.gca().set_aspect('equal', adjustable='box')
+        for v in self.vehicles:
+            self.ax.add_patch(v.circle)
+            self.ax.add_patch(v.triangle)
 
-        self.initScene(ax)
-
+    def showScene(self, animate=False):
         if animate:
             anim = animation.FuncAnimation(
-                fig, self.animate, interval=INTERVAL, blit=True)
+                self.fig, self.animate, interval=INTERVAL, blit=True)
 
         plt.show()
 
@@ -69,11 +65,8 @@ class LightWorld(SimpleWorld):
     def __init__(self, x, y, vehicles=[], light=None):
         super().__init__(x, y, vehicles)
         self.light = light
-
-    def initScene(self, ax):
-        super().initScene(ax)
         if self.light:
-            ax.pcolormesh(self.light.getIntensityField(
+            self.ax.pcolormesh(self.light.getIntensityField(
                 self.width, self.height))
 
     def animate(self, i):
@@ -91,11 +84,8 @@ class LightObstacleWorld(LightWorld):
     def __init__(self, x, y, vehicles=[], light=None, obstacles=[]):
         super().__init__(x, y, vehicles, light)
         self.obstacles = obstacles
-
-    def initScene(self, ax):
-        super().initScene(ax)
         for o in self.obstacles:
-            ax.add_patch(o.patch)
+            self.ax.add_patch(o.patch)
 
     def freeSpace(self, x, y, r):
         result = True
